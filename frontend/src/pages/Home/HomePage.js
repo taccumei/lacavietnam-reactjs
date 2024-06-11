51f5f1ba66dc83f758d2803e3186633e4098ce6a
taccumei <dangngoctram1903@gmail.com>
2024-06-11 13:31:51 -0700

    Adding Search

--- frontend/src/pages/Home/HomePage.js
+++ frontend/src/pages/Home/HomePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useReducer } from 'react'
-import { getAll } from '../../services/foodService';
+import { getAll, search } from '../../services/foodService';
 import Thumbnails from '../../components/Header/Thumbnails/Thumbnails';
+import { useParams } from 'react-router-dom';
+import Search from '../../components/Search/Search';
 
 
 const initialState = { foods: [] };
@@ -17,13 +19,16 @@ const reducer = (state, action) => {
 export default function HomePage() {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { foods } = state;
+  const { searchTerm } = useParams();
 
   useEffect(() => {
-    getAll().then(foods => dispatch({ type: 'FOODS_LOADED', payload: foods }, []));
+    const loadedFoods = searchTerm ? search(searchTerm) : getAll();
+    loadedFoods.then(foods => dispatch({ type: 'FOODS_LOADED', payload: foods }, [searchTerm]));
 })
 
   return (
     <>
+      <Search/>
       <Thumbnails foods={foods} />
     </>
   )
