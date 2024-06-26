857517530c6f9df841472d7752cf52e338e2769b
taccumei <dangngoctram1903@gmail.com>
2024-06-26 13:25:32 -0700

    Loading

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,11 +9,13 @@ import './axiosconfig';
 import { AuthProvider } from './hooks/useAuth';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { LoadingProvider } from './hooks/useLoading';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <LoadingProvider>
       <AuthProvider>
         <CartProvider>
           <App />
@@ -30,7 +32,8 @@ root.render(
                 theme="light"
           />
         </CartProvider>
-      </AuthProvider>
+        </AuthProvider>
+      </LoadingProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
